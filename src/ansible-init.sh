a431c95a500680750888a22c090312f22577820a
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-01-30 14:21:16 +0100

    Allow installation of roles from Ansible Galaxy

--- src/ansible-init.sh
+++ src/ansible-init.sh
@@ -15,17 +15,25 @@ set -u # abort on unbound variable
 
 usage() {
 cat << _EOF_
-Usage: ${0} PROJECT_NAME
+Usage: ${0} PROJECT_NAME [ROLE]...
+  Initialises a Vagrant+Ansible project based on
+  https://github.com/bertvv/ansible-skeleton
+  and optionally, installs the specified roles from Ansible Galaxy
 _EOF_
 }
 
 #}}}
 #{{{ Command line parsing
 
-if [ "$#" -ne "1" ]; then
-    echo "Expected 1 argument, got $#" >&2
+if [ "$#" -lt "1" ]; then
+    echo "Expected at least 1 argument, got $#" >&2
     usage
     exit 2
+fi
+
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+  usage
+  exit 0
 fi
 
 project="$1"
@@ -35,13 +43,22 @@ if [ -d "${project}" ]; then
   exit 1
 fi
 
+shift
+
 #}}}
 # Script proper
 
 wget https://github.com/bertvv/ansible-skeleton/archive/master.zip
 unzip master.zip
+rm master.zip
+
 mv ansible-skeleton-master "${project}"
 git init "${project}"
 cd "${project}"
 git add .
 git commit --message "Initial commit, Ansible skeleton"
+
+for role in "${@}"; do
+  ansible-galaxy install -p ansible/roles "${role}"
+done
+
