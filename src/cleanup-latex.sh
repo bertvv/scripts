044a8d8da4859b3ab168fd9bc272aea35fe5c70b
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-02-22 21:59:35 +0100

    Added script to clean up LaTeX artifacts

---
+++ src/cleanup-latex.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+#
+# Author: Bert Van Vreckem <bert.vanvreckem@gmail.com>
+#
+# Clean up LaTeX help files
+
+rm -v ./*.aux ./*.log ./*.nav ./*.out ./*.snm ./*.toc \
+  ./*.bbl ./*.blg ./*.idx ./*.ilg ./*.ind ./*.tcp ./*.vrb
+
