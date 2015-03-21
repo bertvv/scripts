ced698870effe200def251097ae9266b48ce4aaf
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-03-21 20:10:01 +0100

    Scripts for copying audio cd’s (without ripping individual songs)

---
+++ src/cd2img.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/bash
+#
+# Author:   Bert Van Vreckem <bert.vanvreckem@gmail.com>
+#
+# Read an Audio CD and create an image using cdrdao
+
+# {{{Preamble
+set -e # abort on nonzero exitstatus
+set -u # abort on unbound variable
+
+# {{{ Functions
+
+usage() {
+cat << _EOF_
+Usage: ${0} TITLE
+  Reads an audio cd in disc-at-once mode using cdrdao and creates an image file
+  and associated table of contents with the specified TITLE. Spaces in the
+  title will be replaced with underscores.
+_EOF_
+}
+
+#}}}
+ #{{{ Command line parsing
+
+if [[ "$#" -ne "1" ]]; then
+    echo "Expected 1 argument, got $#" >&2
+    usage
+    exit 2
+fi
+
+#}}}
+#{{{ Variables
+
+title="${1// /_}"
+
+#}}}
+# }}}
+
+# Script proper
+
+cdrdao read-cd --with-cddb --datafile "${title}".img "${title}".toc
+eject
---
+++ src/img2cd.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/bash
+#
+# Author:   Bert Van Vreckem <bert.vanvreckem@gmail.com>
+#
+# Burn audio cd image to disk
+
+set -e # abort on nonzero exitstatus
+set -u # abort on unbound variable
+
+#{{{ Functions
+
+usage() {
+cat << _EOF_
+Usage: ${0} TOCFILE
+  Burn an audio image to CD, identified by the given TOC file
+_EOF_
+}
+
+#}}}
+#{{{ Command line parsing
+
+if [[ "$#" -ne "1" ]]; then
+    echo "Expected 1 argument, got $#" >&2
+    usage
+    exit 2
+fi
+
+#}}}
+#{{{ Variables
+
+toc="$1"
+
+#}}}
+# Script proper
+
+sudo cdrdao write --eject --driver generic-mmc-raw "${toc}"
