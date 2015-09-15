2af41e9828d201566e9689273d2ec91822f53645
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-09-15 19:55:33 +0200

    Update shortpw settings

--- src/shortpw.sh
+++ src/shortpw.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 # Created:  di 28 jun 2011 11:34:59  CEST
-# Modified: Wed 14 Dec 2011 11:02:52 AM CET
 # Author:   Bert Van Vreckem <bert.vanvreckem@gmail.com>
 #
-apg -m 8 -x 8 -n 100 -M NL
+apg -m 8 -x 8 -n 10 -M CNL
