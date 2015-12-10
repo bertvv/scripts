6a69036740f302982756eb9de1c8d0e5f61ee8cb
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-12-11 00:45:40 +0100

    Updated documentation

--- src/ignite.sh
+++ src/ignite.sh
@@ -5,6 +5,9 @@
 # Script to assist an Ignite talk. PDFs in the current directory starting with
 # a number of 2 digits ar shown in order. Every slide is shown 15 seconds.
 # Press ENTER between two presentations.
+#
+# You should install impressive (http://impressive.sourceforge.net/) to run this
+# script.
 
 set -o errexit # abort on nonzero exitstatus
 set -o nounset # abort on unbound variable
