6ce475ec470dddad27d160db178dccd6d5fc0a43
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-12-11 00:40:48 +0100

    Added script for running ignite presentations

---
+++ src/ignite.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash
+#
+# Author: Bert Van Vreckem <bert.vanvreckem@gmail.com>
+#
+# Script to assist an Ignite talk. PDFs in the current directory starting with
+# a number of 2 digits ar shown in order. Every slide is shown 15 seconds.
+# Press ENTER between two presentations.
+
+set -o errexit # abort on nonzero exitstatus
+set -o nounset # abort on unbound variable
+
+for presentation in [0-9][0-9]*.pdf; do
+
+  echo "Press ENTER to start ${presentation}"
+  read
+
+  impressive --nologo \
+    -a 15 \
+    --page-progress \
+    --fade \
+    --autoquit \
+    "${presentation}"
+done
