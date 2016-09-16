1dba30ff84ac7099aca33e631556fc8be4222db0
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2016-09-16 23:00:26 +0200

    Code cleanup, allow searching in multiple directories

--- src/course-links.sh
+++ src/course-links.sh
@@ -20,27 +20,109 @@
 set -e # abort on nonzero exitstatus
 set -u # abort on unbound variable
 
- #{{{ Variables
-top_dir="${HOME}/Documents/Vakken/"
-ac_year="15-16"
+#{{{ Variables
+readonly top_dirs="${HOME}/Documents/Vakken/ ${HOME}/Documents/Avondschool/"
+readonly ac_year="16-17"
 
-bookmark_dir="${HOME}/c/"
+readonly bookmark_dir="${HOME}/c/"
+noop="false"
 #}}}
+#{{{ Functions
+
+# Print usage message on stdout
+usage() {
+cat << _EOF_
+Usage: ${0} [OPTION]
+
+  Creates a symlink to all course directories for the current academic year to
+  a bookmark directory.
+
+OPTIONS:
+
+  -h, -?, --help
+      prints this help message
+  -d, -n, --dry-run, --noop
+      only shows the links that would be created
+_EOF_
+}
+
+# Usage: die MESSAGE
+#
+# Prints a message to stderr and exits with a nonzero exit status
+die() {
+  printf "Error: %s ${*}\n" 1>&2
+  usage
+  exit 1
+}
+
+process_command_line_args() {
+  [ "${#}" -eq "0" ] && return
+
+  case "${1}" in
+    "-h"|"-?"|"--help")
+      usage
+      exit 0
+      ;;
+    "-d"|"-n"|"--dry-run"|"--noop")
+      noop="true"
+      ;;
+    *)
+      die "Invalid option/argument."
+      ;;
+  esac
+}
+
+
+
+# Usage: cleanup_bookmark_dir
+#
+# Remove existing links in bookmark dir
+cleanup_bookmark_dir() {
+  if [[ -d "${bookmark_dir}" ]]; then
+    rm --recursive --force "${bookmark_dir}"
+  fi
+  mkdir --parents "${bookmark_dir}"
+}
+
+# Usage: course_dirs_in DIR
+#
+# searches for directories matching ${ac_year} in the specified directory.
+course_dirs_in() {
+  local top_dir="${1}"
+  find "${top_dir}" -type d -name "${ac_year}"
+}
+
+# Usage: create_links_for DIR
+#
+# Creates links in ${bookmarks_dir} for all course directories found in the
+# specified top directory
+create_links_for() {
+  local top_dir="${1}"
+  local course_year_dirs
+  course_year_dirs=$(course_dirs_in "${top_dir}")
+
+  for course_year_dir in ${course_year_dirs}; do
+    # strip ${ac_year} from end of path
+    course_dir="${course_year_dir%/${ac_year}}"
+
+    # strip top directory from start of path, leaving only the course name
+    course_name="${course_dir#${top_dir}}"
+    bookmark="${bookmark_dir}${course_name}"
+
+    if [ "${noop}" = "true" ]; then
+      echo "${course_year_dir} -> ${bookmark}"
+    else
+      ln --symbolic --force --verbose "${course_year_dir}" "${bookmark}"
+    fi
+  done
+}
+#}}}
+
 # Script proper
+process_command_line_args "${@}"
 
-# Cleanup current bookmark dir
-if [[ -d "${bookmark_dir}" ]]; then
-  rm --recursive --force "${bookmark_dir}"
-fi
-mkdir --parents "${bookmark_dir}"
+cleanup_bookmark_dir
 
-# Create bookmarks
-for course_year_dir in $(find "${top_dir}" -type d -name "${ac_year}"); do
-  course_dir="${course_year_dir%/${ac_year}}"
-  course_name="${course_dir#${top_dir}}"
-  bookmark="${bookmark_dir}${course_name}"
-  #echo "${course_year_dir} -> ${bookmark}"
-  ln --symbolic --force --verbose "${course_year_dir}" "${bookmark}"
+for top_dir in ${top_dirs}; do
+  create_links_for "${top_dir}"
 done
-
-
