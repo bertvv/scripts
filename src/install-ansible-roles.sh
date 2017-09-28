9458b6694a4ae7acc5ae33f2f55dcccf8329a991
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2017-09-28 11:34:36 +0200

    Add script that installs Ansible roles into ~/.ansible

---
+++ src/install-ansible-roles.sh
@@ -0,0 +1,86 @@
+#! /usr/bin/env bash
+#
+# Author: Bert Van Vreckem <bert.vanvreckem@gmail.com>
+#
+# Install the specified Ansible roles in ~/.ansible/roles
+
+#{{{ Bash settings
+# abort on nonzero exitstatus
+set -o errexit
+# abort on unbound variable
+set -o nounset
+# don't hide errors within pipes
+set -o pipefail
+#}}}
+#{{{ Variables
+readonly script_name=$(basename "${0}")
+readonly script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+IFS=$'\t\n'   # Split on newlines and tabs (but not on spaces)
+
+readonly roles='bertvv.bind
+bertvv.dhcp
+bertvv.dnsmasq
+bertvv.httpd
+bertvv.mariadb
+bertvv.pxeserver
+bertvv.rh-base
+bertvv.samba
+bertvv.tftp
+bertvv.tomcat
+bertvv.vsftpd
+bertvv.wordpress'
+
+#}}}
+
+main() {
+  debug "Options: ${*}"
+  install_roles "${@}"
+
+}
+
+#{{{ Helper functions
+
+install_roles() {
+  for role in ${roles}; do
+     info "${role}"
+     install_role "${role}" "${@}"
+  done
+}
+
+install_role() {
+  local role="${1}"
+  shift
+
+  ansible-galaxy install --force "${@}" "${role}"
+}
+
+# Color definitions
+readonly reset='\e[0m'
+readonly cyan='\e[0;36m'
+readonly red='\e[0;31m'
+readonly yellow='\e[0;33m'
+
+# Usage: info [ARG]...
+#
+# Prints all arguments on the standard output stream
+info() {
+  printf "${yellow}>>> %s${reset}\n" "${*}"
+}
+
+# Usage: debug [ARG]...
+#
+# Prints all arguments on the standard output stream
+debug() {
+  printf "${cyan}### %s${reset}\n" "${*}"
+}
+
+# Usage: error [ARG]...
+#
+# Prints all arguments on the standard error stream
+error() {
+  printf "${red}!!! %s${reset}\n" "${*}" 1>&2
+}
+#}}}
+
+main "${@}"
+
