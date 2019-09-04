f89b15b00caee8e122403ff85d4a41e233da974c
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2019-09-05 00:12:54 +0200

    Also remove .lol files, add -f option

--- src/cleanup-latex.sh
+++ src/cleanup-latex.sh
@@ -4,6 +4,6 @@
 #
 # Clean up LaTeX help files
 
-rm -v ./*.{bak,aux,log,nav,out,snm,ptc,toc,bbl,blg,idx,ilg,ind,tcp,vrb,tps,lof,log,lot,synctex.gz,fls,fdb_latexmk,bcf,run.xml} 2> /dev/null
-rm -vr _minted* 2> /dev/null # directory for Pygments syntax coloring
+rm -vf ./*.{bak,aux,log,nav,out,snm,ptc,toc,bbl,blg,idx,ilg,ind,tcp,vrb,tps,lof,log,lol,lot,synctex.gz,fls,fdb_latexmk,bcf,run.xml} 2> /dev/null
+rm -vrf _minted* 2> /dev/null # directory for Pygments syntax coloring
 
