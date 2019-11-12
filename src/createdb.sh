eaf24d2c2cfceab3f2195e6e1d38076f9bdf9f78
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2019-11-12 11:19:13 +0100

    Fix shellcheck warnings

--- src/createdb.sh
+++ src/createdb.sh
@@ -2,22 +2,22 @@
 #
 # Author:   Bert Van Vreckem <bert.vanvreckem@gmail.com>
 #
-SERVER="tin2fbo.hogent.be"
-USER="bert"
+server="tin2fbo.hogent.be"
+user="bert"
 
-DB_NAME="testdb"
-DB_USER="${DB_NAME}_usr"
-DB_PASS="testpw"
+db_name="testdb"
+db_user="${db_name}_usr"
+db_pass="testpw"
 
-SQL=$( cat << _END_
-DROP USER ${DB_USER};
-DROP DATABASE ${DB_NAME};
-CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-GRANT ALL ON ${DB_NAME} TO '${DB_USER}'@'*' IDENTIFIED BY '${DB_PASS}';
+sql=$( cat << _END_
+DROP USER IF EXISTS ${db_user};
+DROP DATABASE IF EXISTS ${db_name};
+CREATE DATABASE ${db_name};
+GRANT ALL ON ${db_name} TO '${db_user}'@'*' IDENTIFIED BY '${db_pass}';
 FLUSH PRIVILEGES;
 _END_
 )
 
-echo $SQL
-ssh ${USER}@${SERVER} "mysql -u root -p ${SQL}"
+echo "${sql}"
+ssh ${user}@${server} "mysql -u root -p ${sql}"
 
