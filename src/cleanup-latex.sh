b25f016e6ec17a2c0b579153e87b5c70d9f351ac
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2022-11-18 16:09:59 +0100

    LaTeX cleanup script: allow target directory as command-line argument

--- src/cleanup-latex.sh
+++ src/cleanup-latex.sh
@@ -4,6 +4,12 @@
 #
 # Clean up LaTeX help files
 
-rm -vf ./*.{bak,aux,log,nav,out,snm,ptc,toc,bbl,blg,idx,ilg,ind,tcp,vrb,tps,lof,log,lol,lot,synctex.gz,fls,fdb_latexmk,bcf,run.xml,xdv} 2> /dev/null
-rm -vrf _minted* 2> /dev/null # directory for Pygments syntax coloring
+if [ $# -ge '1' ] && [ -d "${1}" ]; then
+  dir="${1}"
+else
+  dir="${PWD}"
+fi
+
+rm -vf "${dir}"/*.{bak,aux,log,nav,out,snm,ptc,toc,bbl,blg,idx,ilg,ind,tcp,vrb,tps,lof,log,lol,lot,synctex.gz,fls,fdb_latexmk,bcf,run.xml,xdv} 2> /dev/null
+rm -vrf "${dir}"/_minted* 2> /dev/null # directory for Pygments syntax coloring
 
